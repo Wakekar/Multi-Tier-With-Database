139c27f391ff68416bdfbb6b30af91d8c7f804db
Aniket DevOps <aniket241192@gmail.com>
2026-03-15 15:47:48 +0530

    Update output.tf

--- Terraform/output.tf
+++ Terraform/output.tf
@@ -1,15 +1,22 @@
 output "cluster_id" {
-  value = aws_eks_cluster.aniket.id
+  value       = aws_eks_cluster.aniket.id
+  description = "The ID of the EKS cluster"
 }
 
 output "node_group_id" {
-  value = aws_eks_node_group.aniket.id
+  value       = aws_eks_node_group.aniket.id
+  description = "The ID of the EKS node group"
 }
 
 output "vpc_id" {
-  value = data.aws_vpc.aniket_vpc.id
+  value       = data.aws_vpc.aniket_vpc.id
+  description = "The ID of the existing VPC"
 }
 
 output "subnet_ids" {
-  value = [data.aws_subnet.aniket_subnet.id]
+  value       = [
+    data.aws_subnet.aniket_subnet_1.id,
+    data.aws_subnet.aniket_subnet_2.id
+  ]
+  description = "IDs of the subnets used by the EKS cluster"
 }
