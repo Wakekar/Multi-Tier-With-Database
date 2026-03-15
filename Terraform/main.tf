ae8bee2b404a57c904cb17bc10f85e1900ce6c78
Aniket DevOps <aniket241192@gmail.com>
2026-03-15 15:58:56 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -2,16 +2,12 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-##########################
 # Use existing VPC
-##########################
 data "aws_vpc" "aniket_vpc" {
   id = "vpc-087bcd22867e1366f"
 }
 
-##########################
-# Existing Subnets (one from each AZ)
-##########################
+# Existing subnets in different AZs
 data "aws_subnet" "aniket_subnet_1" {
   id = "subnet-06c612ff09bcab060"  # ap-south-1a
 }
@@ -20,16 +16,52 @@ data "aws_subnet" "aniket_subnet_2" {
   id = "subnet-0142550db89a86499"  # ap-south-1b
 }
 
-##########################
-# Existing Security Group
-##########################
+# Existing Security Group for EKS
 data "aws_security_group" "aniket_cluster_sg" {
   id = "sg-0ac52e60282081cd9"
 }
 
-##########################
+# EKS Cluster
+resource "aws_eks_cluster" "aniket" {
+  name     = "aniket-eks-cluster"
+  role_arn = aws_iam_role.aniket_cluster_role.arn
+
+  vpc_config {
+    subnet_ids         = [data.aws_subnet.aniket_subnet_1.id, data.aws_subnet.aniket_subnet_2.id]
+    security_group_ids = [data.aws_security_group.aniket_cluster_sg.id]
+  }
+
+  tags = {
+    Name = "aniket-eks-cluster"
+  }
+}
+
+# EKS Node Group
+resource "aws_eks_node_group" "aniket" {
+  cluster_name    = aws_eks_cluster.aniket.name
+  node_group_name = "aniket-node-group"
+  node_role_arn   = aws_iam_role.aniket_node_group_role.arn
+  subnet_ids      = [data.aws_subnet.aniket_subnet_1.id, data.aws_subnet.aniket_subnet_2.id]
+
+  scaling_config {
+    desired_size = 3
+    max_size     = 3
+    min_size     = 3
+  }
+
+  instance_types = ["t2.large"]
+
+  remote_access {
+    ec2_ssh_key               = var.ssh_key_name
+    source_security_group_ids = [data.aws_security_group.aniket_cluster_sg.id]
+  }
+
+  tags = {
+    Name = "aniket-node-group"
+  }
+}
+
 # IAM Role for EKS Cluster
-##########################
 resource "aws_iam_role" "aniket_cluster_role" {
   name = "aniket-eks-cluster-role"
 
@@ -54,9 +86,7 @@ resource "aws_iam_role_policy_attachment" "aniket_cluster_role_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
 
-##########################
 # IAM Role for EKS Node Group
-##########################
 resource "aws_iam_role" "aniket_node_group_role" {
   name = "aniket-node-group-role"
 
@@ -89,54 +119,4 @@ resource "aws_iam_role_policy_attachment" "aniket_node_group_cni_policy" {
 resource "aws_iam_role_policy_attachment" "aniket_node_group_registry_policy" {
   role       = aws_iam_role.aniket_node_group_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-}
-
-##########################
-# EKS Cluster
-##########################
-resource "aws_eks_cluster" "aniket" {
-  name     = "aniket-eks-cluster"
-  role_arn = aws_iam_role.aniket_cluster_role.arn
-
-  vpc_config {
-    subnet_ids         = [
-      data.aws_subnet.aniket_subnet_1.id,
-      data.aws_subnet.aniket_subnet_2.id
-    ]
-    security_group_ids = [data.aws_security_group.aniket_cluster_sg.id]
-  }
-
-  tags = {
-    Name = "aniket-eks-cluster"
-  }
-}
-
-##########################
-# EKS Node Group
-##########################
-resource "aws_eks_node_group" "aniket" {
-  cluster_name    = aws_eks_cluster.aniket.name
-  node_group_name = "aniket-node-group"
-  node_role_arn   = aws_iam_role.aniket_node_group_role.arn
-  subnet_ids      = [
-    data.aws_subnet.aniket_subnet_1.id,
-    data.aws_subnet.aniket_subnet_2.id
-  ]
-
-  scaling_config {
-    desired_size = 3
-    max_size     = 3
-    min_size     = 3
-  }
-
-  instance_types = ["t2.large"]
-
-  remote_access {
-    ec2_ssh_key               = var.ssh_key_name
-    source_security_group_ids = [data.aws_security_group.aniket_cluster_sg.id]
-  }
-
-  tags = {
-    Name = "aniket-node-group"
-  }
 }
