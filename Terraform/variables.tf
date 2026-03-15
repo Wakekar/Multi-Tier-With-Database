80da8698c38a79c45a34d3d257e678c0b68ca8f5
Aniket DevOps <aniket241192@gmail.com>
2026-03-15 15:54:00 +0530

    Update variables.tf

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,11 +1,5 @@
 variable "ssh_key_name" {
-  description = "The name of the SSH key pair to use for EKS node group instances"
+  description = "The name of the SSH key pair to use for instances"
   type        = string
   default     = "thinkpad-2"
-}
-
-variable "cluster_name" {
-  description = "Name of the EKS cluster"
-  type        = string
-  default     = "aniket-eks-cluster"
 }
