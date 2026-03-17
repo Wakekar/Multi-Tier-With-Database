34f9497f0d744327303d70e0332432260d5e78af
Aniket DevOps <aniket241192@gmail.com>
2026-03-17 22:31:46 +0530

    Update output.tf

--- Terraform/output.tf
+++ Terraform/output.tf
@@ -2,17 +2,26 @@ output "cluster_id" {
   value = aws_eks_cluster.aniket.id
 }
 
+output "cluster_endpoint" {
+  value = aws_eks_cluster.aniket.endpoint
+}
+
 output "node_group_id" {
-  value = aws_eks_node_group.aniket_node_group_v3.id
+  value = aws_eks_node_group.node_group.id
 }
 
 output "vpc_id" {
-  value = data.aws_vpc.aniket_vpc.id
+  value = data.aws_vpc.devops_vpc.id
 }
 
 output "subnet_ids" {
   value = [
-    data.aws_subnet.aniket_subnet_1.id,
-    data.aws_subnet.aniket_subnet_2.id
+    data.aws_subnet.private_a.id,
+    data.aws_subnet.private_b.id,
+    data.aws_subnet.private_c.id
   ]
+}
+
+output "cluster_security_group_id" {
+  value = data.aws_security_group.devops_sg.id
 }
